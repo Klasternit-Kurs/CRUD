16597452c3d7df73ff7f957d40e2b015417f7778
Dragan Pepic <d.pepic@hotmail.com>
2020-06-10 20:15:52 +0200

    Add project files.

---
+++ Artikal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+	public class Artikal : INotifyPropertyChanged
+	{
+		public int Sifra { get; set; }
+
+		private string _naziv;
+		public string Naziv 
+		{ 
+			get => _naziv; 
+			set
+			{
+				_naziv = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Naziv"));
+			}
+		}
+
+		private decimal _ucena;
+		public decimal Ucena
+		{
+			get => _ucena;
+			set
+			{
+				_ucena = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Ucena"));
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IzlaznaCena"));
+			}
+		}
+
+		private int _marzaProc;
+		public int MarzaProc 
+		{
+			get => _marzaProc;
+			set
+			{
+				_marzaProc = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MarzaProc"));
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IzlaznaCena"));
+			}
+		}
+
+		public decimal IzlaznaCena { get => Ucena * (1 + (decimal)MarzaProc/100);}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public override string ToString() => $"{Sifra}-{Naziv}";
+
+		public override bool Equals(object obj)
+							=> obj is Artikal a && this.Sifra == a.Sifra;
+
+		public override int GetHashCode()
+		{
+			if (Naziv != null)
+				return Sifra + Naziv.GetHashCode();
+			else
+				return Sifra;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRUD
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ObservableCollection<Artikal> listaArt = new ObservableCollection<Artikal>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			dg.ItemsSource = listaArt;
+
+		}
+
+		private void Dodavanje(object sender, RoutedEventArgs e)
+		{
+			Editor ee = new Editor();
+			ee.Owner = this;
+
+			if (ee.ShowDialog() == true) 
+			{
+				listaArt.Add(ee.DataContext as Artikal);
+			}
+		}
+	}
+}
